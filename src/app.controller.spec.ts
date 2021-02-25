a882e8611c3c5a7b921100d279fc5043a5445c17
Cleverson <cleverson_reisferreira@hotmail.com>
2021-02-25 17:19:04 -0300

    configuring initial test structure

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "TUI Polygons API"', () => {
+      expect(appController.getHello()).toBe('TUI Polygons API');
     });
   });
 });
